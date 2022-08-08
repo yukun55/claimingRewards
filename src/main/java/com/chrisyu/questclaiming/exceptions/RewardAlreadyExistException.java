b70f540dc58df7ed8607d12478a1c5fbcc76bcb7
Chris Yu <yupuqun8@gmail.com>
2022-08-07 23:43:47 -0500

    add more Unit Test modified architecture by reduce components in Controller and Service

--- src/main/java/com/chrisyu/questclaiming/exceptions/RewardAlreadyExistException.java
+++ src/main/java/com/chrisyu/questclaiming/exceptions/RewardAlreadyExistException.java
@@ -3,9 +3,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.CONFLICT)
-public class QuestAlreadyCompletedException extends RuntimeException{
-    //Handle the exception when same quest already completed
-    public QuestAlreadyCompletedException(String message){
+public class RewardAlreadyExistException extends RuntimeException{
+    //Handle the exception when create duplicate Reward
+    public RewardAlreadyExistException(String message){
         super(message);
     }
 }
--- src/main/java/com/chrisyu/questclaiming/exceptions/RewardNotFoundException.java
+++ src/main/java/com/chrisyu/questclaiming/exceptions/RewardNotFoundException.java
@@ -3,9 +3,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class QuestNotFoundException extends RuntimeException{
-    //Handle the exception when quest not found
-    public QuestNotFoundException(String message){
+public class RewardNotFoundException extends RuntimeException{
+    //Handle the exception when Reward not found
+    public RewardNotFoundException(String message){
         super(message);
     }
 }
